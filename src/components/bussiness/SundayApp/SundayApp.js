145c90d8048e8b73f9a429dfc3848c110154217d
Kuba <jkb.kam@gmail.com>
2018-06-05 10:51:29 +0200

    making components

---
+++ src/components/bussiness/SundayApp/SundayApp.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+
+class SundayApp extends Component {
+    render () {
+        return (
+            <p> main page</p>
+        )
+    }
+}
+
+export default SundayApp
